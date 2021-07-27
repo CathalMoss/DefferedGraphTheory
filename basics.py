378c7309c7987efcf592bbef5c573eba8e7452b0
root <root@DESKTOP-B0VVBMQ.localdomain>
2021-07-27 19:42:14 +0100

    Looked at the basics.

---
+++ basics.py
@@ -0,0 +1,21 @@
+# Cathal Moss
+#Python basics
+#2021-07-27
+
+print("Hello World!")
+
+a =1
+b= 1.0
+s = "Hello, world from a string!"
+t = '"Hello" from a different string'
+#u = "\"Hello\", world!"
+
+print(a, b, s, t)
+
+print(type(a))
+
+print(s[1])
+
+#slice
+print(s[3:10:2])
+#start at 3 end at char 10 and skip two everytime
