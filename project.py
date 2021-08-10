e2e90fc4cee0c6cea6988f60772f3853d98ca861
CathalMoss <g00371316@gmit.ie>
2021-08-10 01:55:21 +0100

    counting words from txt file

--- project.py
+++ project.py
@@ -1,14 +1,43 @@
 # Deffered Graph Theory Project
 
-with open('test.txt', 'r') as rf:
-    with open('test_copy.txt', 'w') as wf:
-        for line in rf:
-            wf.write(line)
+# Words for txt file
+f = "data.txt"
+f = open (f, 'r')
+
+#List in word file
+list = [] 
+
+# parse word file into individual words, sort, 
+# and then populate into word list
+for l in f:
+    l = l.split() #individual words
+
+    for w in l:
+        list.append(w) # populate word list with words
+
+list.sort() #alphabetical order
+
+wd = {} #word dictionary for words and counts them
+
+# count words in word list and then populate dictionary
+for w in list:
+    wd[w] =list.count(w)
+
+# print and format words and counts
+print('\n{:^8}{:^8}'.format('Word', 'Count'))
+
+# data is printed
+for w in wd:
+    print('{:^8}{:^8d}'.format(w,wd[w]))
+
+print("")
 
 
 
-
-
+# with open('test.txt', 'r') as rf:
+#     with open('test_copy.txt', 'w') as wf:
+#         for line in rf:
+#             wf.write(line)
 
 # with open('test.txt', 'r') as f:
     
