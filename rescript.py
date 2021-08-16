8dc944b33bf2feb1f76227f7b645ef8f9a08f721
CathalMoss <g00371316@gmit.ie>
2021-08-16 21:51:04 +0100

    project completge

--- rescript.py
+++ rescript.py
@@ -1,16 +1,18 @@
 # Deffered Graph Theory Project
 import argparse
 
+#counter for matches
 counter = 0
 
-parser = argparse.ArgumentParser(description='Add a Regular expression and hit enter')
+# agsparse inserted for to read in filepath and have -h/--help options
+parser = argparse.ArgumentParser(description='Add a Regular expression and a filepath')
 parser.add_argument('firstcommand', metavar='path', type=str,
-                     help='Add Regular Expression and then ...')
+                     help='Add a regular expression, hit space and then ...')
 parser.add_argument('filepath', metavar='path', type=str,
-                     help='Add filepath and hit enter')
+                     help='... add filepath and hit enter')
 args = parser.parse_args()
 
-
+#filepath opener using argparse
 with open(args.filepath, 'r', encoding='utf-8') as f:
     for l in f:
         if args.firstcommand in l:
@@ -41,55 +43,3 @@ with open(args.filepath, 'r', encoding='utf-8') as f:
                 print('{:^8}{:^8d}'.format(w,wd[w]))
 
     print(f"\nCounter:  {counter}")
-        #print(l)
-
-
-
-# Words for txt file
-#f = "input.txt"
-#f = open (f, 'r')
-
-
-
-# with open('test.txt', 'r') as rf:
-#     with open('test_copy.txt', 'w') as wf:
-#         for line in rf:
-#             wf.write(line)
-
-# with open('test.txt', 'r') as f:
-
-    # for line in f:
-    #     print(, end='')
-
-    # size_to_read = 10
-
-    # f_contents = f.read(size_to_read)
-    # print(f_contents, end='')
-
-    # f.seek(0)
-
-    # f_contents = f.read(size_to_read)
-    # print(f_contents)
-
-    # while len(f_contents) > 0:
-    #     print(f_contents, end='*')
-    #     f_contents = f.read(size_to_read)
-
-
-#name = name of test
-# mode = r w a r+
-
-# r = read
-# w = write
-# a = appending
-# r+ = read and write
-#f = open('test.txt', 'r')
-
-# f_contents = f.read() = readsfile
-#  f_contents = f.readlines() = reads all lines in order
-
-
-# with open('test2.txt', 'w') as f:
-#     f.write('Test')
-#     f.seek(0)
-#     f.write('R')
